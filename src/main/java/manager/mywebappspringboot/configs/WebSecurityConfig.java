814ccf715b4c18bdb15e8c749f3ac62626eed442
LAPTOP-UEEQEUCE\Пользователь <agoronik@yandex.ru>
2022-11-16 12:02:50 +0300

    admin@mail.ru 1111

---
+++ src/main/java/manager/mywebappspringboot/configs/WebSecurityConfig.java
@@ -0,0 +1,83 @@
+package manager.mywebappspringboot.configs;
+
+import manager.mywebappspringboot.model.User;
+import manager.mywebappspringboot.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Optional;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final SuccessUserHandler successUserHandler;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserService userService, PasswordEncoder passwordEncoder) {
+        this.successUserHandler = successUserHandler;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/webjars/**")
+                .permitAll();
+
+        http
+                .cors().disable()
+                .csrf().disable();
+
+        http.formLogin()
+                .successHandler(successUserHandler)
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .permitAll();
+        http.logout()
+                .permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .and().csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/login").anonymous()
+                .antMatchers("/user/**").hasAuthority("USER")
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        return (UserDetailsService) email -> {
+            Optional<User> user = userService.findUserByEmail(email);
+            if(user.isEmpty()) {
+                throw new UsernameNotFoundException("No user found");
+            }
+            return user.get();
+        };
+    }
+
+
+}
---
+++ src/main/java/manager/mywebappspringboot/service/UserServiceImp.java
@@ -0,0 +1,95 @@
+package manager.mywebappspringboot.service;
+
+import manager.mywebappspringboot.model.User;
+import manager.mywebappspringboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImp(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        String old_pw = userRepository.findById(user.getId()).get().getPassword();
+        String pw = user.getPassword();
+
+        if (passwordEncoder.matches(pw, old_pw) || pw.equals("")) {
+            User updatedUser = userRepository.findById(user.getId()).get();
+            updatedUser.setFirstName(user.getFirstName());
+            updatedUser.setLastName(user.getLastName());
+            updatedUser.setAge(user.getAge());
+            updatedUser.setEmail(user.getEmail());
+            updatedUser.setRoles(user.getRoles());
+            userRepository.save(updatedUser);
+        } else {
+            String solt = passwordEncoder.encode(pw);
+            System.out.println("Новый пароль ecoded" + solt);
+
+            user.setPassword(passwordEncoder.encode(pw));
+            userRepository.save(user);
+        }
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Transactional
+    @Override
+    public User deleteUserById(Long id) {
+        User user = getUserById(id);
+        userRepository.deleteById(id);
+        return user;
+    }
+
+    @Transactional
+    public Optional<User> findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+
+    @Transactional
+    public User getUserByEmail(String email) {
+        return userRepository.findUserByEmail(email).get();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+}
